9e0324c886a72fc14e4256efca49ffdc9fd4c874
Luiz Eduardo de Oliveira Martins <mastinsluiz@brq.com>
2022-02-25 11:39:48 -0300

    Binding de eventos customizados com @Output e EventEmitter

--- projeto/src/app/app.component.ts
+++ projeto/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'projeto';
+
+  funcionarios:any = [];
+
+  aoAdicionar(funcionario:any) {
+    this.funcionarios.push(funcionario);
+  }
+
 }
--- projeto/src/app/app.module.ts
+++ projeto/src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BemVindoComponent } from './bem-vindo/bem-vindo.component';
+import { FuncionarioCardComponent } from './bem-vindo/funcionario-card/funcionario-card.component';
+import { FuncionarioFormComponent } from './funcionario-form/funcionario-form.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    BemVindoComponent
+    FuncionarioCardComponent,
+    FuncionarioFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
